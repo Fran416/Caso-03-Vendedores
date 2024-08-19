2aed47aa1720827da978f981a18ef76d98d58476
Francisco <f.lizama05@ufromail.cl>
2024-08-19 16:09:19 -0400

    Funcion mostrarMatriz

--- src/main/java/CasoVendedores.java
+++ src/main/java/CasoVendedores.java
@@ -1,14 +1,31 @@
 import java.util.Random;
 
 public class CasoVendedores {
-    public static int[][] generarMatriz(int[][] ventasAnuales) {
-        Random random = new Random();
-        for (int fila = 0; fila<=15;fila++) {
-            for (int columna = 0; columna<=12;columna++) {
-                int numeroAleatorio = random.nextInt(101);
+    public static void main(String[] args) {
+        int[][] ventasAnuales = new int[15][12];
+        rellenarMatriz(ventasAnuales);
+        mostrarMatriz(ventasAnuales);
+    }
+    public static int[][] rellenarMatriz(int[][] ventasAnuales) {
+        Random aleatorio = new Random();
+        for (int fila = 0; fila<15;fila++) {
+            for (int columna = 0; columna<12;columna++) {
+                int numeroAleatorio = aleatorio.nextInt(101);
                 ventasAnuales[fila][columna] = numeroAleatorio;
             }
         }
-        return ventasAnuales;}
+        return ventasAnuales;
+    }
+
+    public static void mostrarMatriz(int[][] ventasAnuales) {
+        for (int fila = 0; fila<15;fila++) {
+            for (int columna = 0; columna<12;columna++) {
+                System.out.print(ventasAnuales[fila][columna]+"|");
+            }
+            System.out.println();
+        }
+
+    }
+
 
 }
