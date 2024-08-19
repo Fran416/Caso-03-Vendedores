f8cb6ae7bf15b20aab4527b77e37347cefb1c81e
BenjaG123 <b.garces01@ufromail.cl>
2024-08-19 16:09:46 -0400

    avance 1

--- src/main/java/CasoVendedores.java
+++ src/main/java/CasoVendedores.java
@@ -1,5 +1,21 @@
-import java.util.random.RandomGenerator;
+import java.util.random;
+import java.lang.Math;
+
 
 public class CasoVendedores {
-    public static String[][] main(String[] args) {}
+    public static void main(String[] args) {
+
+        new ventas == new int[16][12];
+
+
+    }
+
+    public static int[][] rellenarVentas (int[][] ventas) {
+        Random rand = new Random();
+        for(int row = 0; row < 15; row++){
+            for(int column = 0; column < 12; column++){
+
+            }
+        }
+    }
 }
