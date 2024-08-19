aed2e5126af342b6fafdda5a6d78016967990038
Francisco <f.lizama05@ufromail.cl>
2024-08-19 15:58:04 -0400

    Funcion GenerarMatriz agregada

--- src/main/java/CasoVendedores.java
+++ src/main/java/CasoVendedores.java
@@ -1,5 +1,14 @@
-import java.util.random.RandomGenerator;
+import java.util.Random;
 
 public class CasoVendedores {
-    public static String[][] main(String[] args) {}
+    public static int[][] generarMatriz(int[][] ventasAnuales) {
+        Random random = new Random();
+        for (int fila = 0; fila<=15;fila++) {
+            for (int columna = 0; columna<=12;columna++) {
+                int numeroAleatorio = random.nextInt(101);
+                ventasAnuales[fila][columna] = numeroAleatorio;
+            }
+        }
+        return ventasAnuales;}
+
 }
